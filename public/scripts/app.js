f5e379d9351aaf14afb2390603847847fe30728d
Clint Milner <clinton.milner@gmail.com>
2018-03-01 21:44:39 +0000

    getting some logic in there

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,78 @@ console.log('app.js has loaded and is running');
 
 // JSX === JavaScript XML (Facebook)
 
-var template = React.createElement(
-    'h1',
-    null,
-    'Indecision App'
-),
-    appRoot = document.getElementById('app');
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put Your Life in the Hands of a Computer'
+};
 
-ReactDOM.render(template, appRoot);
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'three'
+        )
+    )
+);
+
+var user = {
+    name: 'Clint Milner',
+    age: 36,
+    location: 'Granby, CO'
+};
+
+var userTmpl = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+ReactDOM.render(userTmpl, document.getElementById('app'));
+// ReactDOM.render( template, document.getElementById('app') );
--- src/app.js
+++ src/app.js
@@ -1,8 +1,45 @@
-console.log( 'app.js has loaded and is running' );
+console.log('app.js has loaded and is running');
 
 // JSX === JavaScript XML (Facebook)
 
-let template = <h1>Indecision App</h1>,
-    appRoot = document.getElementById('app');
+let app = {
+    title: 'Indecision App',
+    subtitle: 'Put Your Life in the Hands of a Computer'
+};
 
-ReactDOM.render(template, appRoot);
+let template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </ol>
+    </div>
+);
+
+
+let user = {
+    name: 'Clint Milner',
+    age: 36,
+    location: 'Granby, CO'
+};
+
+
+let userTmpl = (
+    <div>
+        <h1>{ user.name ? user.name : 'Anonymous'}</h1>
+        <p>Age: {user.age}</p>
+        {getLocation(user.location)}
+    </div>
+);
+
+function getLocation(location) {
+    if(location) {
+        return <p>Location: { location }</p>;
+    }
+}
+
+ReactDOM.render(userTmpl, document.getElementById('app'));
+// ReactDOM.render( template, document.getElementById('app') );
