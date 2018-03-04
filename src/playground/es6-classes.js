cbe1f7ab06d96bb601c8cb0e9fc999ca875680bf
Clint Milner <clinton.milner@gmail.com>
2018-03-04 20:54:48 +0000

    cool es6 class stuff

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -19,10 +19,56 @@ class Person {
     }
 }
 
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name,age);
+        this.major = major;
+    }
 
-const me = new Person('Clint Milner',36);
-const you = new Person();
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if( this.hasMajor() ){
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+// traveler extends Person
+// add support for home location
+// override get greeting
+   // if isHome, include it in the message
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation){
+        super(name,age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){ return !!this.homeLocation; }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if( this.hasHomeLocation() ){
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+
+
+const me = new Student('Clint Milner', 36, 'Physics');
+const me2 = new Traveler('Clint Milner', 36, 'Granby');
+const you = new Student();
 
 
 console.info( me.getDescription() );
+console.info( me2.getGreeting() );
 console.info( you.getDescription() );
